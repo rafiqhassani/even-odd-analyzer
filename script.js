755d75292277e78370fdc4103fb1945b1dfed922
rafiqhassani <rafiqhassani106@gmail.com>
2026-02-20 19:48:35 +0500

    New Even-Odd Analyzer upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const numberInput = document.getElementById("numberInput");
+const btn = document.getElementById("btn");
+const evenResult = document.getElementById("evenResult");
+const oddResult = document.getElementById("oddResult");
+document.addEventListener("DOMContentLoaded", function () {
+  btn.addEventListener("click", function () {
+    const numbers = numberInput.value
+      .split(",")
+      .map((num) => Number(num.trim()));
+    const evens = [];
+    const odds = [];
+    for (let i = 0; i < numbers.length; i++) {
+      if (numbers[i] % 2 === 0) {
+        evens.push(numbers[i]);
+      } else {
+        odds.push(numbers[i]);
+      }
+    }
+    evenResult.textContent = "Evens: " + evens.join(", ");
+    oddResult.textContent = "Odds: " + odds.join(", ");
+    evenResult.style.color = "green";
+    oddResult.style.color = "red";
+    evenResult.style.fontWeight = "bold";
+    oddResult.style.fontWeight = "bold";
+  });
+});
